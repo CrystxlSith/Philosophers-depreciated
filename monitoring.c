e9be4fcb574e48e343cbf6eb9580bb3b04634504
CrystalSith <joaquim1810@gmail.com>
2024-08-21 13:56:23 +0200

    eatreach

--- monitoring.c
+++ monitoring.c
@@ -3,76 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   monitoring.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crystal <crystal@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jopfeiff <jopfeiff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 13:54:29 by crystal           #+#    #+#             */
-/*   Updated: 2024/08/20 20:12:58 by crystal          ###   ########.fr       */
+/*   Updated: 2024/08/21 13:03:40 by jopfeiff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philosophers.h"
 
-// int dead_philo_monitoring(t_philo *philo)
-// {
-//     // Verrouillage pour éviter les conditions de course lors de l'accès à last_meal et à d'autres variables partagées
-//     pthread_mutex_lock(&(philo->data->dead_lock));
-
-//     // Calculer le temps écoulé depuis le dernier repas
-//     long int time_since_last_meal = get_current_time() - philo->last_meal;
-
-//     // Vérifier si ce temps dépasse le temps maximal sans manger (t_die)
-//     if (time_since_last_meal > philo->data->t_die)
-//     {
-//         // Déverrouiller avant de retourner car le philosophe est déclaré mort
-//         pthread_mutex_unlock(&(philo->data->dead_lock));
-//         return 1;  // Le philosophe est mort
-//     }
-
-//     // Déverrouiller si le philosophe n'est pas mort
-//     pthread_mutex_unlock(&(philo->data->dead_lock));
-//     return 0;  // Le philosophe est encore en vie
-// }
-
 int dead_philo_monitoring(t_philo *philo)
 {
-if (philo->data == NULL) {
-    fprintf(stderr, "Null pointer detected in dead_philo_monitoring\n");
-    exit(1);
+	pthread_mutex_lock(&(philo->data->dead_lock));
+	long int time_since_last_meal = get_current_time() - philo->last_meal;
+	if (time_since_last_meal > philo->data->t_die)
+	{
+		pthread_mutex_unlock(&(philo->data->dead_lock));
+		return 1;
+	}
+	pthread_mutex_unlock(&(philo->data->dead_lock));
+	return 0;
 }
-
-    pthread_mutex_lock(&philo->data->eat_lock);
-    if ((get_current_time() - philo->last_meal) > philo->data->t_die)
-    {
-        pthread_mutex_unlock(&philo->data->eat_lock);  // Toujours déverrouiller explicitement avant de retourner
-        return 1;
-    }
-    pthread_mutex_unlock(&philo->data->eat_lock);
-    return 0;
-}
-
 
 int	dead_one(t_philo *philo)
 {
 	int	i;
 
 	i = 0;
-	if (!philo || !philo->data) {
+	if (!philo || !philo->data)
+	{
 	  fprintf(stderr, "Null pointer detected in dead_one()\n");
  	   exit(1);
 	}
-
 	while (i < philo[0].data->nb)
 	{
-		if (dead_philo_monitoring(&philo[i]))
+		if (dead_philo_monitoring(&philo[i]) || (philo[i].nb_times_eat == philo[i].data->m_eat))
 		{
 			ft_print("is died of starvation !! 👻\n", &philo[i], philo[i].id);
-			 pthread_mutex_lock(&philo[0].data->dead_lock);
+			pthread_mutex_lock(&philo[0].data->dead_lock);
 			philo[0].data->dead = 1;
 			pthread_mutex_unlock(&philo[0].data->dead_lock);
 				return (1);
 			
 		}
 		i++;
+	}
+	return (0);
+}
+
+int		eat_reached(t_philo *philo)
+{
+	int	complete_meals;
+	int	i;
+
+	i = 0;
+	complete_meals = 0;
+	while (i < philo->data->nb)
+	{
+		if (philo->nb_times_eat == philo->data->m_eat)
+			complete_meals += 1;
+		i++;
+	}
+	if (complete_meals == philo->data->nb)
+	{
+		pthread_mutex_lock(&philo[0].data->dead_lock);
+		philo[0].data->dead = 1;
+		pthread_mutex_unlock(&philo[0].data->dead_lock);
+		return (1);
 	}
 	return (0);
 }
@@ -84,7 +81,7 @@ void	*monitoring(void *data)
 	philo = (t_philo *)data;
 	while (1)
 	{
-		if (dead_one(philo) == 1)
+		if (eat_reached(philo) || dead_one(philo))
 			break ;
 	}
 	return (data);
